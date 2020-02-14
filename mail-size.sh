46a853ee05c2c55782d97a55c3d419c7ca4e18a5
Tom Hardy <wuxmedia@gmail.com>
2020-02-14 10:28:42 +0000

    Update mail-size.sh
    another rewrite.

--- mail-size.sh
+++ mail-size.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
-#tdh@positive-internet.com
+#TDH wuxmedia@gmail.com
 ### testmail
-#EMAIL=tdh@positive-internet.com
+EMAIL=tdh@positive-internet.com
 
 FIND="ionice -c3 $(which find)"
 DU="ionice -c3 $(which du)"
 MAILPATH=/var/lib/vmail/
 SENDMAIL=$(which sendmail)
 FROM=admin@example.com
-SUBJECT="Email archiving needed"
+
+# grab absolute path email domains
+DOM=$($FIND "$MAILPATH" -maxdepth 1 -mindepth 1 -type d)
+
+#grab users from each domain
+USERPATH=$($FIND $DOM -maxdepth 1 -mindepth 1 -type d)
+
+#get users more than 14G
+#LIST=$($DU -sh --threshold=15G $USERPATH)
+
+#go through them and make an email address out of it
+#EMAIL_LIST=$(for i in $(echo "$LIST" ) ; do echo $i | grep -v [0-9]G | awk -F "/" '{ print $6"@"$5}'; done)
+
+#loop through that list and send off a mail for each
+for EMAIL in $EMAIL_LIST
+  do
+SUBJECT="Attention: Your mailbox size is too damn high"
 BODY="
 From: $FROM
 To: $EMAIL
 Cc: $FROM
+Reply-To: $FROM
 
 Dear $EMAIL,
 
@@ -21,21 +38,11 @@ Please apply the email archiving standard procedure as soon as possible.
 Do not hesitate to contact me if you need help applying this procedure.
 Let me know when archiving is completed and I will perform the backup.
 
-Yours, 
-$FROM
+Thanks,
+Admin Team
+
 "
 
-# grab absolute path email domains
-DOM=$($FIND "$MAILPATH" -maxdepth 1 -mindepth 1 -type d)
-
-#grab users from each domain
-USERPATH=$($FIND $DOM -maxdepth 1 -mindepth 1 -type d)
-
-#get users more than 15G
-LIST=$($DU -sh --threshold=15G $USERPATH)
-
-#go through them and make an email address out of it
-EMAIL=$(for i in $(echo "$LIST" ) ; do echo $i | grep -v [0-9]G | awk -F "/" '{ print $6"@"$5}'; done)
-
 #send it all off
-echo "Subject: $SUBJECT" "$BODY" | $SENDMAIL -t
+echo "Subject: $SUBJECT" "$BODY" | $SENDMAIL -f $FROM -t
+done
