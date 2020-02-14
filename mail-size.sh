0b4bbc49da0e8130b7e8d050542f1fc8ab998cfa
Tom Hardy <wuxmedia@gmail.com>
2020-02-14 09:48:03 +0000

    Update mail-size.sh
    rewrote it a bit to use `which` to find the binary and ionice it all

--- mail-size.sh
+++ mail-size.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 #tdh@positive-internet.com
+### testmail
+#EMAIL=tdh@positive-internet.com
+
+FIND="ionice -c3 $(which find)"
+DU="ionice -c3 $(which du)"
 MAILPATH=/var/lib/vmail/
+SENDMAIL=$(which sendmail)
+FROM=admin@example.com
+SUBJECT="Email archiving needed"
+BODY="
+From: $FROM
+To: $EMAIL
+Cc: $FROM
+
+Dear $EMAIL,
+
+The volume of your mailbox is approaching the maximum.
+Please apply the email archiving standard procedure as soon as possible.
+Do not hesitate to contact me if you need help applying this procedure.
+Let me know when archiving is completed and I will perform the backup.
+
+Yours, 
+$FROM
+"
 
 # grab absolute path email domains
-DOM=$(find "$MAILPATH" -maxdepth 1 -mindepth 1 -type d)
+DOM=$($FIND "$MAILPATH" -maxdepth 1 -mindepth 1 -type d)
 
 #grab users from each domain
-USER=$(find $DOM -maxdepth 1 -mindepth 1 -type d)
+USERPATH=$($FIND $DOM -maxdepth 1 -mindepth 1 -type d)
 
-#get users more than 14G
-LIST=$(du -sh --threshold=15G $USER)
+#get users more than 15G
+LIST=$($DU -sh --threshold=15G $USERPATH)
 
 #go through them and make an email address out of it
-EMAIL=$(for i in $(echo "$LIST" ) ; do echo $i | grep -v [0-9]G | awk -F "/" '{ print $6"@"$5}'; done)         
+EMAIL=$(for i in $(echo "$LIST" ) ; do echo $i | grep -v [0-9]G | awk -F "/" '{ print $6"@"$5}'; done)
+
+#send it all off
+echo "Subject: $SUBJECT" "$BODY" | $SENDMAIL -t
